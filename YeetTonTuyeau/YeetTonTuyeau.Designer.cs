1ecfd88c00cf602c49951f0d47994c3cf17b3e68
quoije <titmatt94@gmail.com>
2019-10-08 06:30:26 -0400

    v0.1

---
+++ YeetTonTuyeau/YeetTonTuyeau.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace YeetTonTuyeau
+{
+    partial class YeetTonTuyeau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titreApp = new System.Windows.Forms.Label();
+            this.quitApp = new System.Windows.Forms.Button();
+            this.infoProgress = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.outputBtn = new System.Windows.Forms.Button();
+            this.outputtxt = new System.Windows.Forms.TextBox();
+            this.imageBtn = new System.Windows.Forms.Button();
+            this.imgtxt = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.mp3txt = new System.Windows.Forms.TextBox();
+            this.pceTimer = new System.Windows.Forms.Timer(this.components);
+            this.memeImg = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.memeImg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 20);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "rep par Le journal d\'un podcast";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titreApp
+            // 
+            this.titreApp.AutoSize = true;
+            this.titreApp.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titreApp.Location = new System.Drawing.Point(6, 24);
+            this.titreApp.Name = "titreApp";
+            this.titreApp.Size = new System.Drawing.Size(245, 42);
+            this.titreApp.TabIndex = 30;
+            this.titreApp.Text = "Yeet Ton Tuyeau";
+            this.titreApp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // quitApp
+            // 
+            this.quitApp.Location = new System.Drawing.Point(13, 345);
+            this.quitApp.Name = "quitApp";
+            this.quitApp.Size = new System.Drawing.Size(350, 23);
+            this.quitApp.TabIndex = 29;
+            this.quitApp.Text = "ight imma head out";
+            this.quitApp.UseVisualStyleBackColor = true;
+            this.quitApp.Click += new System.EventHandler(this.QuitApp_Click);
+            // 
+            // infoProgress
+            // 
+            this.infoProgress.Location = new System.Drawing.Point(12, 307);
+            this.infoProgress.Name = "infoProgress";
+            this.infoProgress.ReadOnly = true;
+            this.infoProgress.Size = new System.Drawing.Size(351, 20);
+            this.infoProgress.TabIndex = 28;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 278);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(351, 23);
+            this.progressBar1.TabIndex = 27;
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(288, 235);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 26;
+            this.stopBtn.Text = "stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(12, 235);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertBtn.TabIndex = 25;
+            this.convertBtn.Text = "lets go";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // outputBtn
+            // 
+            this.outputBtn.Location = new System.Drawing.Point(288, 184);
+            this.outputBtn.Name = "outputBtn";
+            this.outputBtn.Size = new System.Drawing.Size(75, 23);
+            this.outputBtn.TabIndex = 24;
+            this.outputBtn.Text = "output";
+            this.outputBtn.UseVisualStyleBackColor = true;
+            this.outputBtn.Click += new System.EventHandler(this.OutputBtn_Click);
+            // 
+            // outputtxt
+            // 
+            this.outputtxt.Location = new System.Drawing.Point(12, 186);
+            this.outputtxt.Name = "outputtxt";
+            this.outputtxt.Size = new System.Drawing.Size(248, 20);
+            this.outputtxt.TabIndex = 23;
+            this.outputtxt.Text = "destination";
+            // 
+            // imageBtn
+            // 
+            this.imageBtn.Location = new System.Drawing.Point(289, 152);
+            this.imageBtn.Name = "imageBtn";
+            this.imageBtn.Size = new System.Drawing.Size(74, 23);
+            this.imageBtn.TabIndex = 22;
+            this.imageBtn.Text = "image";
+            this.imageBtn.UseVisualStyleBackColor = true;
+            this.imageBtn.Click += new System.EventHandler(this.ImageBtn_Click);
+            // 
+            // imgtxt
+            // 
+            this.imgtxt.Location = new System.Drawing.Point(12, 154);
+            this.imgtxt.Name = "imgtxt";
+            this.imgtxt.Size = new System.Drawing.Size(248, 20);
+            this.imgtxt.TabIndex = 21;
+            this.imgtxt.Text = "fichier image";
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(288, 120);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseBtn.TabIndex = 20;
+            this.browseBtn.Text = "audio";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.BrowseBtn_Click);
+            // 
+            // mp3txt
+            // 
+            this.mp3txt.Location = new System.Drawing.Point(12, 122);
+            this.mp3txt.Name = "mp3txt";
+            this.mp3txt.Size = new System.Drawing.Size(248, 20);
+            this.mp3txt.TabIndex = 19;
+            this.mp3txt.Text = "fichier audio";
+            this.mp3txt.TextChanged += new System.EventHandler(this.Mp3txt_TextChanged);
+            // 
+            // pceTimer
+            // 
+            this.pceTimer.Tick += new System.EventHandler(this.PceTimer_Tick);
+            // 
+            // memeImg
+            // 
+            this.memeImg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.memeImg.Image = global::YeetTonTuyeau.Properties.Resources.Annotation_2019_10_06_040409;
+            this.memeImg.Location = new System.Drawing.Point(12, 106);
+            this.memeImg.Name = "memeImg";
+            this.memeImg.Size = new System.Drawing.Size(352, 233);
+            this.memeImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.memeImg.TabIndex = 33;
+            this.memeImg.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::YeetTonTuyeau.Properties.Resources._46491902_570941636657585_5463805245031186432_n__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(264, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 13);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "audio >>> video flv -- pour youtube";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(375, 24);
+            this.menuStrip1.TabIndex = 35;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.aboutToolStripMenuItem.Text = "À propos";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // YeetTonTuyeau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 385);
+            this.Controls.Add(this.memeImg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titreApp);
+            this.Controls.Add(this.quitApp);
+            this.Controls.Add(this.infoProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.outputBtn);
+            this.Controls.Add(this.outputtxt);
+            this.Controls.Add(this.imageBtn);
+            this.Controls.Add(this.imgtxt);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.mp3txt);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "YeetTonTuyeau";
+            this.Text = "YeetTonTuyeau V0.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.YeetTonTuyeau_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.memeImg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label titreApp;
+        private System.Windows.Forms.Button quitApp;
+        private System.Windows.Forms.TextBox infoProgress;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Button outputBtn;
+        private System.Windows.Forms.TextBox outputtxt;
+        private System.Windows.Forms.Button imageBtn;
+        private System.Windows.Forms.TextBox imgtxt;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.TextBox mp3txt;
+        private System.Windows.Forms.Timer pceTimer;
+        private System.Windows.Forms.PictureBox memeImg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ YeetTonTuyeau/aboutApp.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace YeetTonTuyeau
+{
+    partial class aboutApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.aboutText = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 409);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(217, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "q u o i j e";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 349);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(217, 40);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "source disponible sur les internets";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 286);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(217, 63);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "j\'utilise xabe.ffmpeg pour le wrapper mais j\'ai legit aucune idée comment qui fon" +
+    "ctionne";
+            // 
+            // aboutText
+            // 
+            this.aboutText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutText.Location = new System.Drawing.Point(12, 235);
+            this.aboutText.Name = "aboutText";
+            this.aboutText.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.aboutText.Size = new System.Drawing.Size(217, 41);
+            this.aboutText.TabIndex = 6;
+            this.aboutText.Text = "logiciel simple codé tout croche en c#.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::YeetTonTuyeau.Properties.Resources._0014191013_41;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(214, 210);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // aboutApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aboutText);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "aboutApp";
+            this.ShowIcon = false;
+            this.Text = "     yeeeeeeeeeeeeeeeeet";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label aboutText;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
